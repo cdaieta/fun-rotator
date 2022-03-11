9788b3ace75f4ca7556eb3d70c3e26fc1650cdbd
David Aieta <daieta@avioconsulting.com>
2022-03-10 22:58:27 -0300

    mejoras

--- docs/main.js
+++ docs/main.js
@@ -1,35 +1,63 @@
 
 const MAX_FANS = 12;
+const DELAY = 1500;
+
 var audio = document.querySelector("#myAudio");
 var btnAddFan = document.querySelector("#btnAddFan");
 
-var playing = false;
-
 var state = {
-    numFans : 0
+    numFans: 0,
+    audioPlaying : false
 }
 
-function togglePlay(element){
-    if (playing){
+function togglePlay(element) {
+    if (element.getAttribute("moving") == "true") {
+        element.setAttribute("moving", "false");
         element.classList.remove("animationFan");
-        audio.pause();
     }
     else {
         element.classList.remove("animationFan");
         void element.offsetWidth;
         element.classList.add("animationFan");
-        audio.currentTime=0;
+        element.setAttribute("moving", "true");
+    }
+
+    if (atLeastOneMoving()) {
+        audio.currentTime = 0;
         audio.play();
     }
-    playing=!playing;
+    else {
+        audio.pause();
+    }
 }
 
-btnAddFan.addEventListener('click', function(){
+function atLeastOneMoving() {
+    return [...document.querySelector("#game").childNodes].some(x=>x.getAttribute("moving")=="true");
+}
+
+btnAddFan.addEventListener('click', function () {
     if (state.numFans == MAX_FANS) return;
-    
+
     state.numFans++;
     let p = document.createElement("p");
     p.classList.add("fan");
+    p.setAttribute("moving", "false");
     p.addEventListener('click', (e) => togglePlay(e.target));
     document.querySelector("#game").appendChild(p);
 });
+
+setInterval(startMoveFan, DELAY);
+
+function startMoveFan() {
+    if (state.numFans > 0) {
+        let numFan = getRandomInt(state.numFans - 1);
+        let fan = document.querySelector("#game").childNodes[numFan];
+        if (fan.getAttribute("moving") == "false") {
+            togglePlay(fan);
+        }
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
